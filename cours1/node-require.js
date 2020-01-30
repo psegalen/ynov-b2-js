b11ef810bf3e8806cda41337b8329a8b7c35bbb1
Pierre Segalen <pierre.segalen@gmail.com>
2020-01-30 17:26:22 +0100

    Using Node arguments to parametrize the URL

--- cours1/node-require.js
+++ cours1/node-require.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 
+const postId = process.argv[2];
+
 axios
-  .get("https://jsonplaceholder.typicode.com/posts/1")
+  .get("https://jsonplaceholder.typicode.com/posts/" + postId)
   .then(resultat => console.log(resultat.data))
   .catch(error => console.error(error.message));
 
