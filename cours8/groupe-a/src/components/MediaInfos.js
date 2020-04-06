4a8a4c265e77e123a8009396d289b55e106c3a65
Pierre Segalen <pierre.segalen@gmail.com>
2020-04-06 10:32:38 +0200

    Switched MediaInfos component type from Stateful to Stateless

--- cours8/groupe-a/src/components/MediaInfos.js
+++ cours8/groupe-a/src/components/MediaInfos.js
@@ -1,83 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import numeral from "numeral";
 import ApiHelper from "../helpers/ApiHelper";
 import MoviePanel from "./MoviePanel";
 import TvPanel from "./TvPanel";
 import { Link } from "react-router-dom";
 
-class MediaInfos extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      similars: [],
-      isLoading: true,
-    };
-  }
-
-  componentDidMount() {
-    ApiHelper.getSimilars(this.props.type, this.props.media.id).then(
-      (response) =>
-        this.setState({
-          similars: response.data.results,
-          isLoading: false,
-        })
-    );
-  }
-
-  render() {
-    return (
-      <div className="infos">
-        <div className="communes">
-          <div>Synopsis : {this.props.media.overview}</div>
-          <div>
-            Note :{" "}
-            <span
-              style={{
-                color:
-                  this.props.media.vote_average >= 7
-                    ? "rgb(0, 255, 0)"
-                    : this.props.media.vote_average < 5
-                    ? "rgb(255, 0, 0)"
-                    : undefined,
-              }}
-            >
-              {this.props.media.vote_average}
-            </span>{" "}
-            (
-            <span>
-              {numeral(this.props.media.vote_count).format("0,0 a")}
-            </span>{" "}
-            votes)
-          </div>
-          <div>
-            Médias similaires :
-            <ul className="similars">
-              {this.state.similars.map((media) => (
-                <li>
-                  <Link
-                    to={`/${
-                      this.props.type === "movie" ? "film" : "serie"
-                    }/${media.id}`}
-                  >
-                    {this.props.type === "movie"
-                      ? media.title
-                      : media.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+const MediaInfos = (props) => {
+  const [similars, setSimilars] = useState([]);
+  useEffect(() => {
+    ApiHelper.getSimilars(
+      props.type,
+      props.media.id
+    ).then((response) => setSimilars(response.data.results));
+  }, [props.type, props.media.id]);
+  return (
+    <div className="infos">
+      <div className="communes">
+        <div>Synopsis : {props.media.overview}</div>
+        <div>
+          Note :{" "}
+          <span
+            style={{
+              color:
+                props.media.vote_average >= 7
+                  ? "rgb(0, 255, 0)"
+                  : props.media.vote_average < 5
+                  ? "rgb(255, 0, 0)"
+                  : undefined,
+            }}
+          >
+            {props.media.vote_average}
+          </span>{" "}
+          (
+          <span>
+            {numeral(props.media.vote_count).format("0,0 a")}
+          </span>{" "}
+          votes)
         </div>
-        <div id={this.props.type}>
-          {this.props.type === "movie" ? (
-            <MoviePanel media={this.props.media} />
-          ) : (
-            <TvPanel media={this.props.media} />
-          )}
+        <div>
+          Médias similaires :
+          <ul className="similars">
+            {similars.map((media) => (
+              <li>
+                <Link
+                  to={`/${
+                    props.type === "movie" ? "film" : "serie"
+                  }/${media.id}`}
+                >
+                  {props.type === "movie" ? media.title : media.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+      <div id={props.type}>
+        {props.type === "movie" ? (
+          <MoviePanel media={props.media} />
+        ) : (
+          <TvPanel media={props.media} />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default MediaInfos;
